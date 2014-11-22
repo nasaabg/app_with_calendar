5b8fd839c7ae6069b58befe50d909708d36a3979
nasaabg <janek.kurzydlo@gmail.com>
2014-11-23 00:55:12 +0100

    customer validations, form for nested model, fixed datepicker view

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,13 +2,17 @@ class Customer < ActiveRecord::Base
 
   validates_presence_of :name, message: "Proszę podać imię."
   validates_presence_of :surname, message: "Proszę podać nazwisko."
-  validate :email_or_phone
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: "Nieprawidłowy adres e-mail." }, if: :email?
+  validates :phone, format: { with: /[0-9]/, message: "Nieprawidłowe znaki." }
+  validates :phone, length: { in: 6..9, message: "Nieprawidłowy nr telefonu." }, if: :phone?
+  validate :email_or_phone_exist
 
   belongs_to :reservation
 
 
   private
-    def email_or_phone
+
+    def email_or_phone_exist
       email.present? or phone.present? ? true : errors.add(:base, 'Prosze podać telefon lub e-mail.')
     end
 end
